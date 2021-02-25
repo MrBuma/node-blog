084cb47b7e851a479bf1b17cee22e7b21cdc4665
Kiet Bui <hung.buma@gmail.com>
2021-02-25 13:47:51 +0700

    make a model first

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,7 +1,19 @@
+const Course = require('./models/Course');
+
 class SiteController {
 
     home(req, res){
-        res.render('home');
+        //res.render('home');
+        // res.json({
+        //     'name': 'Test thu'
+        // });
+        Course.find({}, function (err, courses) {
+                if(!err){
+                    res.json(courses);
+                }else {
+                    res.status(400).json({err: "Loi"});
+                }
+          });
     }
 
     search(req, res){
--- src/config/db/index.js
+++ src/config/db/index.js
@@ -4,11 +4,8 @@ async function connect(){
 
     try{
         await mongoose.connect('mongodb://127.0.0.1:27017/f8_education_dev', {
-                             useNewUrlParser: true,
+            useNewUrlParser: true,
             useUnifiedTopology: true
-
-
-        
             });
         console.log("connect successfully!");
     } catch(err){
