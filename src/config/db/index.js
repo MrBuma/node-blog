175b21f4258336e3da55340b94f34abb9c633b23
Kiet Bui <hung.buma@gmail.com>
2021-03-06 10:30:51 +0700

    completed delete Module

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -5,7 +5,8 @@ async function connect(){
     try{
         await mongoose.connect('mongodb://127.0.0.1:27017/f8_education_dev', {
             useNewUrlParser: true,
-            useUnifiedTopology: true
+            useUnifiedTopology: true,
+            useCreateIndex: true
             });
         console.log("connect successfully!");
     } catch(err){
